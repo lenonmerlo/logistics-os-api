02781f55621386e7b2e04d3c4fe5267f2fc1868c
lenonmerlo <lenontm@gmail.com>
2026-05-15 14:29:34 -0300

    feat: add users module with entity, DTO, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: config.get<string>('NODE_ENV') !== 'production',
       }),
     }),
+    UsersModule,
   ],
 })
 export class AppModule {}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+// import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() CreateUserDto: CreateUserDto) {
+    return this.usersService.create(CreateUserDto);
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findById(id);
+  }
+}
